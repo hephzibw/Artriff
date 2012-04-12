8bae1bf4e3df3e52ac83ac0f2842ca01e0876ec9
Hephzibah <hephzibah@Hephzibah.local>
2012-04-12 23:55:25 +0530

    first commit--default directory structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Artriff::Application.config.secret_token = '2ed9dbce9c6292d653dbc01817ffa4545a18ba5bed19c9108a4f96418476bb5f4f00722a6ca92037f1a405e7a9d08420adc07e6f843ceb2b609ddd0908170925'
