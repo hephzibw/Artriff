ab6317992aa26793405ea9ddcd1874ae5b128d77
Hephzibah <hephzibah.1989@gmail.com>
2012-05-23 22:36:38 +0530

    index and show actions for categories

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -10,11 +10,21 @@ describe CategoriesController do
       response.should be_success
     end
 
-    it "should be successfull" do
+    it "should list out all the categories" do
       ["Photographers", "Musicians"].each { |category_name| Category.create!(:name => category_name) }
       get :index
       response.body.should have_selector('h1', text: Category.first.name)
       response.body.should have_selector('h1', text: Category.last.name)
+    end
+  end
+
+  describe "show" do
+    it "should be successfull" do
+      c = FactoryGirl.create(:category, :name => "abc")
+      get :show, {:id => c.id}
+      response.should be_success
+      p response.body
+      response.body.should have_selector('p', text: "abc")
     end
   end
 end
